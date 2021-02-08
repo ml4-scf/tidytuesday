e2942c5060e4e3fa3635dc15a6c05483a218462b
Max Lay <maxlay92@googlemail.com>
2021-02-08 21:07:17 +0100

    Initial set up of project

---
+++ 2021_02_08_INEQ/script/Inequality.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+tuesdata <- tidytuesdayR::tt_load('2021-02-09')
+
+lifetime_earn <- tuesdata$lifetime_earn
+
+student_debt <- tuesdata$student_debt
+retirement <- tuesdata$retirement
+home_owner <- tuesdata$home_owner
+race_wealth <- tuesdata$race_wealth
+income_time <- tuesdata$income_time
+income_limits <- tuesdata$income_limits
+income_aggregate <- tuesdata$income_aggregate
+income_distribution <- tuesdata$income_distribution
+income_mean <- tuesdata$income_mean
+
+
+
